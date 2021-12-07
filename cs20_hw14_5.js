796474b9a25d874e10b6aa5eb3aba7dcf5a3cddd
ajmiller00 <47611503+ajmiller00@users.noreply.github.com>
2021-12-07 11:14:34 -0500

    Add files via upload

---
+++ cs20_hw14_5.js
@@ -0,0 +1,72 @@
+var http = require('http');
+var fs = require('fs');
+var qs = require('querystring');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://amille26:cs20hw14@cluster0.ocphp.mongodb.net/stockticker?retryWrites=true&w=majority";
+
+// var port = process.env.PORT || 3000;
+var port = 8080;
+  
+http.createServer(function (req, res) {
+    adr = req.url
+    
+    if (adr == "/") {
+    	file = 'cs20_hw14_2.html';
+    	fs.readFile(file, function(err, txt) {
+	    	res.writeHead(200, {'Content-Type': 'text/html'});
+	    	res.write(txt);
+    	});
+
+	    pdata = "";
+	    req.on('data', data => {
+	         pdata += data.toString();
+	    });
+
+	    MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+	        if(err) { console.log("Connection err: " + err); return; }
+	        
+	        var dbo = db.db("stockticker");
+	        var coll = dbo.collection('companies');
+
+	        theQuery = "";
+	        if (pdata['rad'] == 'symbol' && input != "") {
+	          theQuery = {ticker: input};
+	        }
+	        else if (pdata['rad'] == 'name' && input != "") {
+	          theQuery = {company: input};
+	        }
+
+	        if (theQuery != "") {
+	          	coll.find(theQuery).toArray(function(err, items) {
+		            if (err) {
+			            console.log("Error: " + err);
+						res.end();
+				  		db.close();
+		            } else {
+			        	for (i=0; i<items.length; i++)
+			            	res.write("<br>" + (i+1) + ". <br>Company Name: " + items[i].company + "<br>Ticker Symbol: " + items[i].ticker + "<br>");      
+		            }
+		            if (items.length == 0) {
+		            	res.write("<br>No match for input");
+		            }
+					res.end();
+		  			db.close(); 
+	        	});
+	        } else {
+				res.end();
+	  			db.close(); 
+			}
+	    });
+
+	    req.on('end', () => {
+	    	pdata = qs.parse(pdata);
+	    	global.input = pdata['user_input'];
+	    });
+
+    } else {
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.write ("Unknown page request");
+      res.end();
+    }
+
+}).listen(port);
